a06b7b4512c9a6a478b28021480a32796b5efa65
Yining Wang <yining.wang@utexas.edu>
2019-10-19 20:49:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,8 +8,8 @@ def map(arr)
 end
 
 def reduce(arr, start = 0)
-  for i in 0...arr.length - 1 do
-    result = yield(arr[i], arr[i + 1])
+  for i in 0...arr.length do
+    result = yield(result, arr[i])
   end
   result += start if start != 0
   result
